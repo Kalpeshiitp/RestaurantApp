eb6f2782a4da87d875b90fbcf17c095306e8c63f
Kalpeshiitp <114363318+Kalpeshiitp@users.noreply.github.com>
2022-12-18 12:12:57 +0530

    initial code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import Resturant from './components/Basics/Resturant';
+
+const App = () => {
+  return <Resturant/>
+}
+
+export default App;
